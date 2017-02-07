1abbfc521a795f90f5412bc6167075e9ae0f3600
JimGray1 <ywendeng@126.com>
2017-02-07 22:55:08 +0800

    使用Runnable 实现多线程
    1.使用Runnable 创建的多线程来处理同一份资源

--- src/cn/ywdeng/basic/thread/ThreadWithExtends.java
+++ src/cn/ywdeng/basic/thread/ThreadWithExtends.java
@@ -9,32 +9,34 @@ import java.util.Random;
  * @Description: 使用继承的方式创建多线程
  */
 public class ThreadWithExtends extends Thread {
-	//设置一个变量
-	private String flag=null;
-	public ThreadWithExtends(String flag){
-		this.flag=flag;
+	// 设置一个变量
+	private String flag = null;
+
+	public ThreadWithExtends(String flag) {
+		this.flag = flag;
 	}
-	
-	public void run(){
-	 String threadName=Thread.currentThread().getName();
-	 System.out.println(threadName+"线程的run 方法被调用.....");
-	 Random random=new Random();
-	 for (int i =0;i<=20; i++){
-		 //使线程随机睡眠一段时间
-		 try {
-			Thread.sleep(random.nextInt(10)*100);
-			System.out.println(threadName+"  "+flag);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+
+	public void run() {
+		String threadName = Thread.currentThread().getName();
+		System.out.println(threadName + "线程的run 方法被调用.....");
+		Random random = new Random();
+		for (int i = 0; i <= 20; i++) {
+			// 使线程随机睡眠一段时间
+			try {
+				Thread.sleep(random.nextInt(10) * 100);
+				System.out.println(threadName + "  " + flag);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
-	 }
-	 
+
 	}
-   public static void main(String[] args) {
-  ThreadWithExtends	thread1=new ThreadWithExtends("线程1");
-  ThreadWithExtends thread2=new  ThreadWithExtends("线程2");
-  //启动线程
-  thread1.start();
-  thread2.start();
-}
+
+	public static void main(String[] args) {
+		ThreadWithExtends thread1 = new ThreadWithExtends("线程1");
+		ThreadWithExtends thread2 = new ThreadWithExtends("线程2");
+		// 启动线程
+		thread1.start();
+		thread2.start();
+	}
 }
--- src/cn/ywdeng/basic/thread/ThreadWithImpliment.java
+++ src/cn/ywdeng/basic/thread/ThreadWithImpliment.java
@@ -1,30 +1,50 @@
 package cn.ywdeng.basic.thread;
+
+import java.util.Random;
+
 /**
  * @author ywdeng
  * @date 2017年2月6日
  * @Title: ThreadWithImpliment.java
- * @Description: 使用实现接口的方式来实现多线程
+ * @Description: 使用实现接口的方式来实现多线程-----使用多线程来100售票
  */
 public class ThreadWithImpliment implements Runnable{
+	private  int tickets=100;
+	//此处flag作为状态标志，在sychronized 中一个线程对其的修改能够保证另一个线程的可见性
+	private boolean flag=true;
 
 	@Override
 	public void run() {
-	  String threadName=Thread.currentThread().getName();
-	  System.out.println("线程 "+threadName+" 调用run()方法");
-	  for(int i=0;i<20;i++){
-		  System.out.println(threadName+" "+i);
-		  try {
-			Thread.sleep(1000);
+	 String threadName=Thread.currentThread().getName();
+
+	  while(flag){
+	   synchronized (this) {
+	   // 如果还有剩余的票，则就售票
+	   if (tickets>0){
+	    System.out.println("线程 "+threadName+" 出售了第"+(100-tickets+1)+" 张票");
+	        --tickets;
+	    System.out.println("当前系统还剩余"+tickets+"张票");
+	   }else 
+		  //如果票已经售完之后跳出循环
+		  flag=false;
+	    }
+	   
+		try {
+			//使得当前线程随机睡上一定时间
+			Thread.sleep(new Random().nextInt(100));
 		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 	  }
-	}
+	  }
+	
  public static void main(String[] args) {
-	//在创建线程对象的时候和继承Thread方法不同=------使用的静态代理模式
-	 Thread th1=new Thread(new ThreadWithImpliment(),"线程1");
-	 Thread th2=new Thread(new ThreadWithImpliment(),"线程2");
+	//创建一个Runnnable接口实现类对象，并将其传入Thread类中创建多个线程，实现多个
+	//线程对同一份资源的共享
+	 ThreadWithImpliment t=new ThreadWithImpliment();
+	 Thread th1=new Thread(t,"线程1");
+	 Thread th2=new Thread(t,"线程2");
 	 th1.start();
 	 th2.start();
 }
